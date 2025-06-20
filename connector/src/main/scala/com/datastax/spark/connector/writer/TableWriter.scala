68bbb282581c4a6467a7e1edfc2a91ddf42d7efd
willmpls <will.fraley@gmail.com>
2025-06-19 21:31:39 -0500

    SPARKC-715 fix TTL and timestamp options for updates and deletes

--- connector/src/main/scala/com/datastax/spark/connector/writer/TableWriter.scala
+++ connector/src/main/scala/com/datastax/spark/connector/writer/TableWriter.scala
@@ -91,9 +91,35 @@ class TableWriter[T] private (
     val deleteColumnsClause = deleteColumnNames.map(quote).mkString(", ")
     val whereClause = quotedColumnNames(primaryKey).map(c => s"$c = :$c").mkString(" AND ")
 
-    s"DELETE ${deleteColumnsClause} FROM ${quote(keyspaceName)}.${quote(tableName)} WHERE $whereClause"
+    val usingTimestampClause = timestampSpec.map(ts => s"USING $ts").getOrElse("")
+    if (ttlEnabled)
+      logWarning(s"${writeConf.ttl} is ignored for DELETE query")
+
+    s"DELETE ${deleteColumnsClause} FROM ${quote(keyspaceName)}.${quote(tableName)} $usingTimestampClause WHERE $whereClause"
   }
-  private lazy val queryTemplateUsingUpdate: String = {
+
+  private lazy val timestampSpec: Option[String] = {
+    writeConf.timestamp match {
+      case TimestampOption(PerRowWriteOptionValue(placeholder)) => Some(s"TIMESTAMP :$placeholder")
+      case TimestampOption(StaticWriteOptionValue(value)) => Some(s"TIMESTAMP $value")
+      case _ => None
+    }
+  }
+
+  private lazy val ttlEnabled: Boolean = writeConf.ttl != TTLOption.defaultValue
+
+  private lazy val optionsSpec: String = {
+    val ttlSpec = writeConf.ttl match {
+      case TTLOption(PerRowWriteOptionValue(placeholder)) => Some(s"TTL :$placeholder")
+      case TTLOption(StaticWriteOptionValue(value)) => Some(s"TTL $value")
+      case _ => None
+    }
+
+    val options = List(ttlSpec, timestampSpec).flatten
+    if (options.nonEmpty) s"USING ${options.mkString(" AND ")}" else ""
+  }
+
+  private[connector] lazy val queryTemplateUsingUpdate: String = {
     val (primaryKey, regularColumns) = columns.partition(_.isPrimaryKeyColumn)
     val (counterColumns, nonCounterColumns) = regularColumns.partition(_.isCounterColumn)
 
@@ -118,7 +144,7 @@ class TableWriter[T] private (
     val setClause = (setNonCounterColumnsClause ++ setCounterColumnsClause).mkString(", ")
     val whereClause = quotedColumnNames(primaryKey).map(c => s"$c = :$c").mkString(" AND ")
 
-    s"UPDATE ${quote(keyspaceName)}.${quote(tableName)} SET $setClause WHERE $whereClause"
+    s"UPDATE ${quote(keyspaceName)}.${quote(tableName)} $optionsSpec SET $setClause WHERE $whereClause"
   }
 
   private val isCounterUpdate =
